399e0cdec64a3663d5d3f939b90c29d693415d5c
magnusgbjerke <magnusgbjerke@gmail.com>
2025-02-01 19:05:29 +0100

    move index.css to styles folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Test from './test/Test.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "../styles/index.css";
+import Test from "./test/Test.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Test />
-  </StrictMode>,
-)
+  </StrictMode>
+);
