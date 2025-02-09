0250f1cad6c6cf528b1ef9e4d7bf1c0d9a22191c
Daniel Ingebrigtsen <149110359+Gubbagubbax@users.noreply.github.com>
2025-02-09 12:33:44 +0100

    update Checkbox and Breadcrumbs (#10)

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,13 +1,17 @@
 interface Props {
   checked?: boolean;
+  onChange: () => void;
 }
 
-export function Checkbox({ checked = false }: Props) {
+export function Checkbox({ checked = false, onChange }: Props) {
   const baseStyles =
     "border border-gray-300 rounded focus:outline-none focus:ring-blue-500 placeholder-gray-400";
   return (
-    <>
-      <input type="checkbox" checked={checked} className={`${baseStyles}`} />
-    </>
+    <input
+      type="checkbox"
+      checked={checked}
+      onChange={onChange}
+      className={baseStyles}
+    />
   );
 }
