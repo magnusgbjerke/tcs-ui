b453d537e6b94054bc28cc8cdf4ffef4fab21963
magnusgbjerke <magnusgbjerke@gmail.com>
2025-02-09 12:09:51 +0100

    update

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -6,10 +6,11 @@ export function Checkbox({ checked = false }: Props) {
   const baseStyles =
     "border border-gray-300 rounded focus:outline-none focus:ring-blue-500 placeholder-gray-400";
   return (
-    <div className="ml-4">
-      <input type="checkbox" checked={checked} className={baseStyles} />
-      <span className="mx-2"></span>
-      <input type="checkbox" className={baseStyles} />
-    </div>
+    <input
+      type="checkbox"
+      checked={checked}
+      onChange={() => !checked}
+      className={baseStyles}
+    />
   );
 }
