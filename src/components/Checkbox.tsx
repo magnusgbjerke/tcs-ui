21c5490328e20b5ca95c21bbd45d7c03d83c1424
magnusgbjerke <magnusgbjerke@gmail.com>
2025-02-09 12:27:47 +0100

    update

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,15 +1,16 @@
 interface Props {
   checked?: boolean;
+  onChange: () => void;
 }
 
-export function Checkbox({ checked = false }: Props) {
+export function Checkbox({ checked = false, onChange }: Props) {
   const baseStyles =
     "border border-gray-300 rounded focus:outline-none focus:ring-blue-500 placeholder-gray-400";
   return (
     <input
       type="checkbox"
       checked={checked}
-      onChange={() => !checked}
+      onChange={onChange}
       className={baseStyles}
     />
   );
--- src/tester/displays/CheckboxDisplay.tsx
+++ src/tester/displays/CheckboxDisplay.tsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import { Checkbox } from "../../components/Checkbox";
 
 export function CheckboxDisplay() {
+  const [state, setState] = useState(false);
   return (
     <>
       <p className="text-3xl underline p-2">Checkbox</p>
-      <Checkbox checked={true} />
+      <Checkbox checked={state} onChange={() => setState(!state)} />
     </>
   );
 }
